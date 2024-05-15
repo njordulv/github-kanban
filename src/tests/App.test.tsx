ec2015ba96c5b7b36a15842f42d01c3406ad104f
njordulv <njordulv@gmail.com>
2024-05-15 13:47:43 +0300

    UPD: App tests & snapshot

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,19 +2,18 @@ import { render, screen } from '@testing-library/react'
 import { App } from 'components/App/App'
 
 describe('App component', () => {
-  it('Renders "Done" & "Progress" texts', () => {
+  it('renders "Done" & "Progress" texts', () => {
     render(<App />)
 
     const DoneElement = screen.getByText(/Done/i)
-    const ProgressElement = screen.getByText(/Progress/i)
-
     expect(DoneElement).toBeInTheDocument()
+
+    const ProgressElement = screen.getByText(/Progress/i)
     expect(ProgressElement).toBeInTheDocument()
   })
 
-  it('If renders correctly', () => {
-    const AppSnapshot = render(<App />)
-
-    expect(AppSnapshot).toMatchSnapshot()
+  it('renders correctly according to the snapshot', () => {
+    const { asFragment } = render(<App />)
+    expect(asFragment()).toMatchSnapshot()
   })
 })
